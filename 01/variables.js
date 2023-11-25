147d2916a1226d20baac0db5abe702deb932e2e9
Subhashini2711 <79558838+Subhashini2711@users.noreply.github.com>
2023-11-25 16:40:53 +0000

    container config done

---
+++ 01/variables.js
@@ -0,0 +1,15 @@
+const stuId=101 //canot change value
+let stuEmail="shubhashini2001@ymail.com" //variable declaration
+var stuPassword="12345" //variable declaration
+stuCity="Mangalore" //doesnt need a variable type declaration
+let stuState //undefined without further assigning
+//stuId=102
+stuEmail="suha@hamd.com"
+stuPassword="23451"
+stuCity="Bengaluru"
+console.log(stuId);
+console.table([stuId,stuEmail,stuPassword,stuCity.stuState])
+
+/*
+due to scopes dont use var use let instead
+*/
---
+++ 02/datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //to treat js as new js version
+/*
+alert("hello")
+alert(3+3)
+*/
+console.log(3+3);
+console.log("hello")
+
+/*
+datatypes in javascript
+string ->" "
+number ->2 to power^53
+bigint
+boolean
+null ->stand alone value
+undefined ->value not assigned
+symbol ->unique
+
+object
+*/
+console.log(typeof "4")
