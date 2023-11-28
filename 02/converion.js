75d84c40d761960a180267ce6d7f4bd054f7b9f4
Subhashini2711 <79558838+Subhashini2711@users.noreply.github.com>
2023-11-28 17:40:39 +0000

    conversion

--- 02/converion.js
+++ 02/converion.js
@@ -10,3 +10,16 @@ console.log(typeof(score1))
 
 // "string to number is easy ("33" to 33)"
 //33abc->33 not possible
+
+
+/*operations*/
+
+let value=9
+let negValue=-value
+console.log(negValue);//changing negative
+
+let s1="good"
+let s2="Morning"
+str=s1+s2
+console.log(str)//concatinating
+
