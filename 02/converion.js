cc8c776ce6e74c64ffec8ead919b37ebde8d5d20
Subhashini2711 <79558838+Subhashini2711@users.noreply.github.com>
2023-11-28 17:26:59 +0000

    datatype conversion

---
+++ 02/converion.js
@@ -0,0 +1,12 @@
+let score="36"
+let score1=2
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(score1)
+console.log(typeof(score1))
+
+// "string to number is easy ("33" to 33)"
+//33abc->33 not possible
