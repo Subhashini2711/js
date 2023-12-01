4d08ab0f79bfcfeebde53e9c8ab7b7784da65768
Subhashini2711 <79558838+Subhashini2711@users.noreply.github.com>
2023-12-01 17:11:03 +0000

    math and num

---
+++ 02/num_math.js
@@ -0,0 +1,16 @@
+const score=400
+const balance=new Number(100)
+console.log(balance);
+
+const hundreads=1000000
+console.log(hundreads.toLocaleString('en-In'));
+
+
+//Maths
+
+console.log(Math);
+console.log(Math.abs(-40));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.6));
+
+console.log(Math.random());
